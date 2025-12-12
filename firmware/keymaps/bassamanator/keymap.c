bccecd324e7211b22ba704de5d43798fa6ee0a86
Bassam Husain <bassam.husain@gmail.com>
2025-12-12 01:00:23 -0500

    adjust enthium layer

--- firmware/keymaps/bassamanator/keymap.c
+++ firmware/keymaps/bassamanator/keymap.c
@@ -53,10 +53,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [_ENTHIUM] = LAYOUT(
         KC_TAB,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_BSPC,
-        QK_GESC,   KC_Q,    KC_Y,    KC_O,    KC_U,    KC_EQL,                            KC_X,    KC_L,    KC_D,    KC_W,    KC_Z,     KC_BSLS,
+        KC_NO,   KC_Q,    KC_Y,    KC_O,    KC_U,    KC_EQL,                            KC_X,    KC_L,    KC_D,    KC_W,    KC_Z,     KC_BSLS,
         KC_B,     GUI_C_E, ALT_I_E, CTL_A_E, SFT_E_E, KC_MINS,                           KC_K,    SFT_H_E, CTL_T_E, ALT_N_E, GUI_S_E,  KC_F,
-        KC_CAPS,  KC_QUOT, KC_COMM, KC_DOT,  KC_SCLN, KC_SLSH,                          KC_J,    KC_M,    KC_G,    KC_P,    KC_V,     KC_NO,
-                                            KC_NO, KC_NO, KC_SPC,              KC_ENT,  KC_R, KC_NO
+        KC_CAPS,  KC_QUOT, KC_COMM, KC_DOT,  KC_SCLN, KC_SLSH,                          KC_J,    KC_M,    KC_G,    KC_P,    KC_V,     KC_ENT,
+                                            KC_NO, KC_NO, KC_SPC,              QK_GESC,  KC_R, KC_NO
     ),
     [_FUN] = LAYOUT(
         KC_GRV,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                              KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,

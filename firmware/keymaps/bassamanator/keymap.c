a4f46f7cffe422c003dbebfeeb3a3a5c809f45ed
Bassam Husain <bassam.husain@gmail.com>
2025-12-14 21:58:42 -0500

    adjust nav

--- firmware/keymaps/bassamanator/keymap.c
+++ firmware/keymaps/bassamanator/keymap.c
@@ -61,11 +61,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                             KC_NO   ,   MO(1), KC_SPC ,         QK_GESC , KC_R   , QK_LEAD
     ),
     [_NAV] = LAYOUT(
-        KC_NO   , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,                            KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_TRNS,
+        KC_NO   , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,                            KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_BSPC,
         KC_NO   , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,                            KC_PGUP, KC_HOME, KC_UP  , KC_END , KC_PSCR, KC_NO  ,
         KC_NO   , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,                            KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_NO  , KC_NO  ,
-        KC_NO   , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,                            KC_TRNS, KC_DEL , KC_INS , KC_NO  , KC_NO  , KC_TRNS,
-                                            KC_NO   , KC_NO  , KC_TRNS,         KC_TRNS , KC_NO  , QK_LLCK
+        KC_NO   , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,                            KC_NO  , KC_DEL , KC_INS , KC_NO  , KC_NO  , KC_ENT,
+                                            KC_NO   , KC_NO  , KC_SPC ,         QK_GESC , KC_NO  , QK_LLCK
     ),
     [_FUN] = LAYOUT(
         KC_GRV,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                              KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,

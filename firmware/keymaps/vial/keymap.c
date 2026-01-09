127a46459f5e04ae7b520f85c85178a53e254fc0
Bassam Husain <bassam.husain@gmail.com>
2026-01-08 22:39:05 -0500

    adjustment for enthium-v13 complete

--- firmware/keymaps/vial/keymap.c
+++ firmware/keymaps/vial/keymap.c
@@ -55,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GRV  , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                            KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_BSPC,
         KC_TAB  , KC_Q   , LT1_Y_E, LT3_O_E, KC_U   , KC_EQL ,                            LT5_X_E, KC_L   , LT2_D_E, LT4_W_E, LT6_Z_E, KC_BSLS,
         KC_B    , GUI_C_E, ALT_I_E, CTL_A_E, SFT_E_E, KC_MINS,                            KC_K   , SFT_H_E, CTL_T_E, ALT_N_E, GUI_S_E, KC_F   ,
-        KC_DEL  , KC_QUOT, KC_COMM, KC_DOT , KC_SCLN, KC_SLSH,                            KC_J   , KC_M   , KC_G   , KC_P   , LT7_V_E, KC_ENT ,
+        KC_DEL  , KC_QUOT, KC_COMM, KC_DOT , KC_SLSH, KC_SCLN,                            KC_J   , KC_M   , KC_G   , KC_P   , LT7_V_E, KC_ENT ,
                                             KC_ESC  ,KC_CAPS , KC_SPC ,           KC_SPC  , KC_R , KC_NO
     ),
     [_NAV] = LAYOUT(
